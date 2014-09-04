d49108eda38d3b3eb39d59e20173ec8eec4bb775
Nestor Soriano Vilchez <nestor.soriano@sunhotels.net>
2014-09-04 17:17:41 +0200

    Added the tests project, with a (mostly empty yet) class for testing the PlainMemory class.

---
+++ Main.Tests/PlainMemoryTests.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using Ploeh.AutoFixture;
+
+namespace Konamiman.Z80dotNet.Tests
+{
+    public class PlainMemoryTests
+    {
+        private int MemorySize { get; set; }
+        private PlainMemory Sut { get; set; }
+        private Fixture Fixture { get; set; }
+
+        [SetUp]
+        public void Setup()
+        {
+            Fixture = new Fixture();
+            MemorySize = Fixture.Create<int>();
+            Sut = new PlainMemory(MemorySize);
+        }
+
+        [Test]
+        public void Can_create_instances()
+        {
+            Assert.IsNotNull(Sut);
+        }
+    }
+}
