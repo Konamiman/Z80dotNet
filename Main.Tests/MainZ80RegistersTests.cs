b3d1ef12487cdf481aeccb2028e585e197dd0f63
Nestor Soriano Vilchez <nestor.soriano@sunhotels.net>
2014-09-05 10:11:23 +0200

    Added the ShortExtensions class with tests.

---
+++ Main.Tests/MainZ80RegistersTests.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+using Ploeh.AutoFixture;
+
+namespace Konamiman.Z80dotNet.Tests
+{
+    public class MainZ80RegistersTests
+    {
+        private Fixture Fixture { get; set; }
+        MainZ80Registers Sut { get; set; }
+
+        [SetUp]
+        public void Setup()
+        {
+            Fixture = new Fixture();
+            Sut = new MainZ80Registers();
+        }
+
+        [Test]
+        public void Gets_A_correctly_from_AF()
+        {
+            var A = Fixture.Create<byte>();
+            var F = Fixture.Create<byte>();
+            var AF = (short)((A << 8) + F);
+
+            Sut.AF = AF;
+
+            var actual = Sut.A;
+            Assert.AreEqual(A, actual);
+        }
+    }
+}
