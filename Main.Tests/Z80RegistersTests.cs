9d8b61604ad615a712eb536a9fa87ecbc177b19e
Nestor Soriano Vilchez <nestor.soriano@sunhotels.net>
2014-09-05 11:29:17 +0200

    Added the IXh, IXy, IYh, IYl, I and R registers to the Z80Registers class.

---
+++ Main.Tests/Z80RegistersTests.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using Ploeh.AutoFixture;
+
+namespace Konamiman.Z80dotNet.Tests
+{
+    public class Z80RegistersTests
+    {
+        private Fixture Fixture { get; set; }
+        Z80Registers Sut { get; set; }
+
+        [SetUp]
+        public void Setup()
+        {
+            Fixture = new Fixture();
+            Sut = new Z80Registers();
+        }
+
+        [Test]
+        public void Gets_IXh_and_IXl_correctly_from_IX()
+        {
+            var IXh = Fixture.Create<byte>();
+            var IXl = Fixture.Create<byte>();
+            var IX = NumberUtils.CreateShort(IXh, IXl);
+
+            Sut.IX = IX;
+
+            Assert.AreEqual(IXh, Sut.IXh);
+            Assert.AreEqual(IXl, Sut.IXl);
+        }
+
+        [Test]
+        public void Sets_IX_correctly_from_IXh_and_IXl()
+        {
+            var IXh = Fixture.Create<byte>();
+            var IXl = Fixture.Create<byte>();
+            var expected = NumberUtils.CreateShort(IXh, IXl);
+
+            Sut.IXh = IXh;
+            Sut.IXl = IXl;
+
+            Assert.AreEqual(expected, Sut.IX);
+        }
+
+        [Test]
+        public void Gets_IYh_and_IYl_correctly_from_IY()
+        {
+            var IYh = Fixture.Create<byte>();
+            var IYl = Fixture.Create<byte>();
+            var IY = NumberUtils.CreateShort(IYh, IYl);
+
+            Sut.IY = IY;
+
+            Assert.AreEqual(IYh, Sut.IYh);
+            Assert.AreEqual(IYl, Sut.IYl);
+        }
+
+        [Test]
+        public void Sets_IY_correctly_from_IYh_and_IYl()
+        {
+            var IYh = Fixture.Create<byte>();
+            var IYl = Fixture.Create<byte>();
+            var expected = NumberUtils.CreateShort(IYh, IYl);
+
+            Sut.IYh = IYh;
+            Sut.IYl = IYl;
+
+            Assert.AreEqual(expected, Sut.IY);
+        }
+
+        [Test]
+        public void Gets_I_and_R_correctly_from_IR()
+        {
+            var I = Fixture.Create<byte>();
+            var R = Fixture.Create<byte>();
+            var IR = NumberUtils.CreateShort(I, R);
+
+            Sut.IR = IR;
+
+            Assert.AreEqual(I, Sut.I);
+            Assert.AreEqual(R, Sut.R);
+        }
+
+        [Test]
+        public void Sets_IR_correctly_from_I_and_R()
+        {
+            var I = Fixture.Create<byte>();
+            var R = Fixture.Create<byte>();
+            var expected = NumberUtils.CreateShort(I, R);
+
+            Sut.I = I;
+            Sut.R = R;
+
+            Assert.AreEqual(expected, Sut.IR);
+        }
+    }
+}
--- Main/Z80Registers.cs
+++ Main/Z80Registers.cs
@@ -43,19 +43,109 @@
         /// <summary>
         /// The current interrupt mode register. It has always the value 0, 1 or 2.
         /// </summary>
-        /// <exception cref="System.ArgumentException">Attempt to set a value other than 0, 1 or 2</exception>
+        /// <exception cref="System.InvalidOperationException">Attempt to set a value other than 0, 1 or 2</exception>
         public byte IM { get; set; }    // 0, 1 or 2
 
         /// <summary>
         /// The IFF1 flag. It has always the value 0 or 1.
         /// </summary>
-        /// <exception cref="System.ArgumentException">Attempt to set a value other than 0 or 1</exception>
-        public byte IFF1 { get; set; }  // 0 or 1
+        /// <exception cref="System.InvalidOperationException">Attempt to set a value other than 0 or 1</exception>
+        public int IFF1 { get; set; }  // 0 or 1
 
         /// <summary>
         /// The IFF2 flag. It has always the value 0 or 1.
         /// </summary>
-        /// <exception cref="System.ArgumentException">Attempt to set a value other than 0 or 1</exception>
-        public byte IFF2 { get; set; }  // 0 or 1
+        /// <exception cref="System.InvalidOperationException">Attempt to set a value other than 0 or 1</exception>
+        public int IFF2 { get; set; }  // 0 or 1
+
+        /// <summary>
+        /// The IXh register.
+        /// </summary>
+        public byte IXh
+        {
+            get
+            {
+                return IX.GetHighByte();
+            }
+            set
+            {
+                IX = IX.SetHighByte(value);
+            }
+        }
+
+        /// <summary>
+        /// The IXl register.
+        /// </summary>
+        public byte IXl
+        {
+            get
+            {
+                return IX.GetLowByte();
+            }
+            set
+            {
+                IX = IX.SetLowByte(value);
+            }
+        }
+
+        /// <summary>
+        /// The IYh register.
+        /// </summary>
+        public byte IYh
+        {
+            get
+            {
+                return IY.GetHighByte();
+            }
+            set
+            {
+                IY = IY.SetHighByte(value);
+            }
+        }
+
+        /// <summary>
+        /// The IYl register.
+        /// </summary>
+        public byte IYl
+        {
+            get
+            {
+                return IY.GetLowByte();
+            }
+            set
+            {
+                IY = IY.SetLowByte(value);
+            }
+        }
+
+        /// <summary>
+        /// The I register.
+        /// </summary>
+        public byte I
+        {
+            get
+            {
+                return IR.GetHighByte();
+            }
+            set
+            {
+                IR = IR.SetHighByte(value);
+            }
+        }
+
+        /// <summary>
+        /// The R register.
+        /// </summary>
+        public byte R
+        {
+            get
+            {
+                return IR.GetLowByte();
+            }
+            set
+            {
+                IR = IR.SetLowByte(value);
+            }
+        }
     }
 }
