2847d2199ca4d3428990ab8a4795124c4cbbd8f1
Nestor Soriano Vilchez <nestor.soriano@sunhotels.net>
2014-09-10 08:00:59 +0200

    Added the CancelMemoryAccess property to the MemoryAccessEventArgs class. Readme updated.

--- Main/MemoryAccessEventArgs.cs
+++ Main/MemoryAccessEventArgs.cs
@@ -1,7 +1,7 @@
 namespace Konamiman.Z80dotNet
 {
     /// <summary>
-    /// Event triggered by the <see cref="IZ80Processor"/> class before and after a memory address or port access.
+    /// Event triggered by the <see cref="IZ80Processor"/> class before and after a memory or port access.
     /// </summary>
     public class MemoryAccessEventArgs : ProcessorEventArgs
     {
@@ -16,9 +16,51 @@ namespace Konamiman.Z80dotNet
         public ushort Address { get; private set; }
 
         /// <summary>
-        /// Gets the value that has been read, will be written, or has been written.
-        /// Before a read it does not contain any meaningful value.
+        /// Gets or sets the value that has been read, will be written, or has been written.
         /// </summary>
+        /// <remarks>
+        /// <para>
+        /// The underlying memory manager (the <see cref="IZ80Processor.Memory"/>
+        /// property or the <see cref="IZ80Processor.PortsSpace"/> property of the <see cref="IZ80Processor"/>
+        /// that triggered the event) will not be accessed, and the value of this property when the <c>After*</c>
+        /// event starts will be the same as when the matching <c>Before*</c> method finished
+        /// (see <see cref="EventType"/>), in the following cases:
+        /// </para>
+        /// <list type="bullet">
+        /// <item><description>The access is a memory or port read, and the memory mode for the address
+        /// (see <see cref="IZ80Processor.SetMemoryAccessMode"/>, <see cref="IZ80Processor.GetMemoryAccessMode"/>,
+        /// <see cref="IZ80Processor.SetPortsSpaceAccessMode"/>, <see cref="IZ80Processor.GetPortAccessMode"/>)
+        /// is <see cref="MemoryAccessMode.NotConnected"/> or <see cref="MemoryAccessMode.WriteOnly"/>.
+        /// </description></item>
+        /// <item><description>The access is a memory or port write, and the memory mode for the address
+        /// is <see cref="MemoryAccessMode.NotConnected"/> or <see cref="MemoryAccessMode.ReadOnly"/>.
+        /// </description></item>
+        /// <item><description>The <see cref="CancelMemoryAccess"/> property is set to <b>true</b> during
+        /// the <c>Before*</c> event.
+        /// </description></item>
+        /// </list>
+        /// <para>The value of this property at the beginning of a <c>Before*</c> event for read is always 0xFF.</para>
+        /// </remarks>
         public byte Value { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value that indicates whether access to the underlying memory manager should be cancelled.
+        /// </summary>
+        /// <remarks>
+        /// <para>
+        /// If this property is set to <b>true</b> during a <c>Before*</c> event (see <see cref="EventType"/>),
+        /// then the underlying memory manager (the <see cref="IZ80Processor.Memory"/>
+        /// property or the <see cref="IZ80Processor.PortsSpace"/> property of the <see cref="IZ80Processor"/> that triggered the event)
+        /// will not be accessed. Instead, the matching <c>After*</c> event will be triggered directly, having a
+        /// <see cref="Value"/> equal to the one set in the <c>Before*</c> event.
+        /// </para>
+        /// <para>
+        /// The value of this property when the <c>After*</c> event is triggered is the same that the matching 
+        /// <c>Before*</c> event had when it ended,
+        /// so it is possible to check whether the memory access was cancelled or not. Changing the value
+        /// of this property during the <c>After*</c> event has no effect.
+        /// </para>
+        /// </remarks>
+        public bool CancelMemoryAccess { get; set; }
     }
 }
