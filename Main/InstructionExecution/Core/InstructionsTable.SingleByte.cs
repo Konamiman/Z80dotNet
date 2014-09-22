1c964af06a181e061878415e1d6f247f9d00ff84
Nestor Soriano Vilchez <nestor.soriano@sunhotels.net>
2014-09-22 12:16:36 +0200

    Implemented the LD (HL),n method, such as LD (HL),34h.

--- Main/InstructionExecution/Core/InstructionsTable.SingleByte.cs
+++ Main/InstructionExecution/Core/InstructionsTable.SingleByte.cs
@@ -272,14 +272,6 @@ namespace Konamiman.Z80dotNet
         //TODO: Move instructions to their own files and include them in the table when they are implemented.
 
         /// <summary>
-        /// The LD (HL),n instruction.
-        /// </summary>
-        byte LD_aHL_n()
-        {
-            throw new NotImplementedException();
-        }
-
-        /// <summary>
         /// The LD B,B instruction.
         /// </summary>
         byte LD_B_B()
---
+++ Main/InstructionExecution/Instructions/LD (HL),n    .cs
@@ -0,0 +1,18 @@
+﻿namespace Konamiman.Z80dotNet
+{
+    public partial class Z80InstructionExecutor
+    {
+        /// <summary>
+        /// The LD (nn),A instruction.
+        /// </summary>
+        private byte LD_aHL_n()
+        {
+            var value = ProcessorAgent.FetchNextOpcode();
+            FetchFinished();
+
+            ProcessorAgent.WriteToMemory(Registers.HL.ToUShort(), value);
+
+            return 10;
+        }
+    }
+}
