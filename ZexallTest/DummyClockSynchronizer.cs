03c153fc9a008d23dec5cce6d113f8d17d434b23
Nestor Soriano Vilchez <nestor.soriano@sunhotels.net>
2014-12-24 12:29:13 +0100

    Added the ZexallTest project.

---
+++ ZexallTest/DummyClockSynchronizer.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading;
+
+namespace Konamiman.Z80dotNet.ZexallTest
+{
+    class DummyClockSynchronizer : IClockSynchronizer
+    {
+        public decimal EffectiveClockFrequencyInMHz { get; set; }
+
+        public void Start()
+        {
+        }
+
+        public void Stop()
+        {
+        }
+
+        public void TryWait(int periodLengthInCycles)
+        {
+            Thread.Sleep(1);
+        }
+    }
+}
