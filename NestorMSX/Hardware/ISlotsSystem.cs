a599149509dac3fd4e81442aab3f152c5d05e5ce
Nestor Soriano Vilchez <nestor.soriano@sunhotels.net>
2014-12-29 14:06:45 +0100

    Added the slots system interface.

---
+++ NestorMSX/Hardware/ISlotsSystem.cs
@@ -0,0 +1,22 @@
+﻿using Konamiman.Z80dotNet;
+
+namespace Konamiman.NestorMSX.Hardware
+{
+    /// <summary>
+    /// Represents the slots system of a MSX computer.
+    /// </summary>
+    public interface ISlotsSystem : IMemory
+    {
+        /// <summary>
+        /// Writes the specified value in the slot selection register.
+        /// </summary>
+        /// <param name="value">Value to write</param>
+        void WriteToSlotSelectionRegister(byte value);
+
+        /// <summary>
+        /// Reads the current value of the slot selection register.
+        /// </summary>
+        /// <returns>Current value of the slot selection register</returns>
+        byte ReadSlotSelectionRegister();
+    }
+}
